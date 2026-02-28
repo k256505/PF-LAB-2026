49d7147ccf8e4b4903d27c0ff7cc8623c7030309
k256505 <k256505@nu.edu.pk>
2026-03-01 00:29:20 +0500

    Fix output message for student score input - Task 10

--- Lab 06 Tasks/Task10.c
+++ Lab 06 Tasks/Task10.c
@@ -2,7 +2,7 @@
 int main(void){
     int i = 0,score = 0, dist = 0, pass = 0, fail = 0;
     while(score != -1){
-        printf("\nEnter the score of Student No.%d (1-100): ",++i);
+        printf("\nEnter score of Student No.%d (1-100): ",++i);
         scanf("%d",&score);
         if(score >= 75 && score <= 100){
             dist+=1;
