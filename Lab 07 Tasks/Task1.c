df01dca182570fc02245c2dec026a66c71a7369b
k256505 <k256505@nu.edu.pk>
2026-03-06 22:03:04 +0500

    Ass number shifting program - Task 1

--- Lab 07 Tasks/Task1.c
+++ Lab 07 Tasks/Task1.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
+#include<conio.h>
 int main(void){
     int i,temp,arr[5]={};
     printf("Enter 5 numbers: ");
     for(i=0;i<5;i++){
         scanf("%d",&arr[i]);
+        printf("\033[A\033[17C\033[K");
     }
     for(i=4;i>0;i--){
         temp=arr[i];
         arr[i]=arr[i-1];
         arr[i-1]=temp;
     }
+    printf("\n");
     for(i=0;i<5;i++){
         printf("%d",arr[i]);
     }
