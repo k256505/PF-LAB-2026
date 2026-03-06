61a5fe5547ae0e09bb3934c7cca39cddc3cdb04e
k256505 <k256505@nu.edu.pk>
2026-03-06 10:41:07 +0500

    Add number shifter program - Task 1

--- Lab 07 Tasks/Task1.c
+++ Lab 07 Tasks/Task1.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main(void){
-    int i,temp,arr[]={1,2,3,4,5};
+    int i,temp,arr[5]={};
+    printf("Enter 5 numbers: ");
+    for(i=0;i<5;i++){
+        scanf("%d",&arr[i]);
+    }
     for(i=4;i>0;i--){
         temp=arr[i];
         arr[i]=arr[i-1];
